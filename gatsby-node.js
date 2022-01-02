2343bacfba34ed84454cfcbd67bb67d459b7518a
Petr Schönfeld <petr.schonfeld@gmail.com>
2022-01-02 16:58:49 +0100

    Update

--- gatsby-node.js
+++ gatsby-node.js
@@ -106,7 +106,7 @@ exports.createSchemaCustomization = ({ actions }) => {
       title: String
       description: String
       date: Date @dateformat
-      serving: Number
+      serving: String
     }
 
     type Fields {
